2e85d7f35675d394ef6fc45c83d107afc0543cf3
corsantic <aydofurkan25@gmail.com>
2018-11-22 15:33:22 +0300

    entities one to one jpa relationships

--- src/main/java/enemo/springframework/recipe/RecipeApplication.java
+++ src/main/java/enemo/springframework/recipe/RecipeApplication.java
@@ -9,4 +9,7 @@ public class RecipeApplication {
     public static void main(String[] args) {
         SpringApplication.run(RecipeApplication.class, args);
     }
+
+
+
 }
--- src/main/java/enemo/springframework/recipe/controllers/IndexController.java
+++ src/main/java/enemo/springframework/recipe/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"","/","index"})
     public String getIndexPage(){
+        System.out.println("saying ...  123123213211");
         return "index";
     }
 
