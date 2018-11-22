6a790c3474663220ee1fab54ce0591675232577d
corsantic <aydofurkan25@gmail.com>
2018-11-22 15:49:30 +0300

    unit of measure one to one ingredient

--- src/main/java/enemo/springframework/recipe/domain/Recipe.java
+++ src/main/java/enemo/springframework/recipe/domain/Recipe.java
@@ -1,6 +1,7 @@
 package enemo.springframework.recipe.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -17,6 +18,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
