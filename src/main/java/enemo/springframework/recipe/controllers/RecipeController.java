bcc8de2e1827e9372f94be30ca5f356dc1421033
corsantic <aydofurkan25@gmail.com>
2018-11-22 21:12:31 +0300

    recipes data loader and recipe service and recipe html

---
+++ src/main/java/enemo/springframework/recipe/controllers/RecipeController.java
@@ -0,0 +1,30 @@
+package enemo.springframework.recipe.controllers;
+
+import enemo.springframework.recipe.repositories.RecipeRepository;
+import enemo.springframework.recipe.services.RecipeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RecipeController {
+
+    private RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("/recipes")
+    public String recipeList(Model model) {
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
+
+        return "recipes/index";
+
+
+    }
+
+
+}
---
+++ src/main/java/enemo/springframework/recipe/services/RecipeService.java
@@ -0,0 +1,10 @@
+package enemo.springframework.recipe.services;
+
+import enemo.springframework.recipe.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService  {
+
+    Set<Recipe> getRecipes();
+}
