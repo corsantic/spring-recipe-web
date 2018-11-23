670d4b9645b3f3a4e87e149b4b793e0e447802b2
corsantic <aydofurkan25@gmail.com>
2018-11-23 10:34:17 +0300

    refactoring loader

--- src/main/java/enemo/springframework/recipe/domain/Ingredient.java
+++ src/main/java/enemo/springframework/recipe/domain/Ingredient.java
@@ -21,6 +21,11 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure) {
+        this.description = description;
+        this.amount = amount;
+        this.unitOfMeasure = unitOfMeasure;
+    }
 
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure, Recipe recipe) {
         this.description = description;
--- src/main/java/enemo/springframework/recipe/domain/Recipe.java
+++ src/main/java/enemo/springframework/recipe/domain/Recipe.java
@@ -119,6 +119,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Difficulty getDifficulty() {
@@ -144,4 +145,15 @@ public class Recipe {
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
     }
+
+
+    public Recipe addIngredient(Ingredient ingredient)
+    {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+
+    }
+
+
 }
