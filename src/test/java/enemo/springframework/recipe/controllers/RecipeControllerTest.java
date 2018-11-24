676a9abfd6be111d8f1797e1f053858d636cc77a
corsantic <aydofurkan25@gmail.com>
2018-11-24 14:37:23 +0300

    mocikito argument capture

--- src/test/java/enemo/springframework/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/enemo/springframework/recipe/controllers/RecipeControllerTest.java
@@ -1,11 +1,16 @@
 package enemo.springframework.recipe.controllers;
 
+import enemo.springframework.recipe.domain.Recipe;
 import enemo.springframework.recipe.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -33,13 +38,32 @@ public class RecipeControllerTest {
     @Test
     public void recipeList() {
 
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+
+        recipe.setDescription("new");
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+
+        //when
         String viewName = recipeController.recipeList(model);
 
+        //then
         assertEquals("recipes/index", viewName);
         verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
 
-        verify(model, times(1)).addAttribute(eq("recipes"),anySet());
 
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
 
     }
 }
